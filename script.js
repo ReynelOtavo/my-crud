ab2eeb062105e31fe56a8eb79f3b5fd17fb91ea0
Rey Otavo <144962929+ReynelOtavo@users.noreply.github.com>
2023-11-28 20:33:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,157 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const taskList = document.getElementById('taskList');
-    const taskInput = document.getElementById('taskInput');
-    const addTaskBtn = document.getElementById('addTaskBtn');
-  
-    displayTasks();
-  
-    addTaskBtn.addEventListener('click', addTask);
-  
-    function displayTasks() {
-      taskList.innerHTML = '';
-      let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-      tasks.forEach((task, index) => {
-        const row = `<tr>
-          <td>${index + 1}</td>
-          <td>${task}</td>
-          <td>
-            <button class="editBtn">Editar</button>
-            <button class="deleteBtn">Eliminar</button>
-          </td>
-        </tr>`;
-        taskList.innerHTML += row;
-      });
-  
 
-      const editButtons = document.querySelectorAll('.editBtn');
-      const deleteButtons = document.querySelectorAll('.deleteBtn');
-  
-      editButtons.forEach((button, index) => {
-        button.addEventListener('click', () => editTask(index));
-      });
-  
-      deleteButtons.forEach((button, index) => {
-        button.addEventListener('click', () => deleteTask(index));
-      });
+document.getElementById('visitaForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const nombre = document.getElementById('nombre').value;
+    const direccion = document.getElementById('direccion').value;
+    const email = document.getElementById('email').value;
+    const motivo = document.getElementById('motivo').value;
+
+
+    const visita = {
+        nombre,
+        direccion,
+        email,
+        motivo
+    };
+
+
+    let visitas = JSON.parse(localStorage.getItem('visitas')) || [];
+
+
+    visitas.push(visita);
+
+    localStorage.setItem('visitas', JSON.stringify(visitas));
+
+
+    mostrarVisitas();
+});
+
+function mostrarVisitas() {
+    const visitas = JSON.parse(localStorage.getItem('visitas')) || [];
+
+    const visitasTable = document.getElementById('visitasTable');
+    const tableBody = visitasTable.getElementsByTagName('tbody')[0];
+    tableBody.innerHTML = '';
+
+    visitas.forEach(visita => {
+        const row = tableBody.insertRow();
+        row.innerHTML = `
+            <td>${visita.nombre}</td>
+            <td>${visita.direccion}</td>
+            <td>${visita.email}</td>
+            <td>${visita.motivo}</td>
+        `;
+    });
+}
+
+
+mostrarVisitas();
+
+document.getElementById('visitaForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const nombre = document.getElementById('nombre').value;
+    const direccion = document.getElementById('direccion').value;
+    const email = document.getElementById('email').value;
+    const motivo = document.getElementById('motivo').value;
+
+    const visita = {
+        nombre,
+        direccion,
+        email,
+        motivo
+    };
+
+    let visitas = JSON.parse(localStorage.getItem('visitas')) || [];
+
+
+    localStorage.setItem('visitas', JSON.stringify(visitas));
+
+
+    mostrarVisitas();
+});
+
+function mostrarVisitas() {
+    const visitas = JSON.parse(localStorage.getItem('visitas')) || [];
+
+    const visitasTable = document.getElementById('visitasTable');
+    const tableBody = visitasTable.getElementsByTagName('tbody')[0];
+    tableBody.innerHTML = '';
+
+    visitas.forEach((visita, index) => {
+        const row = tableBody.insertRow();
+        row.innerHTML = `
+            <td>${visita.nombre}</td>
+            <td>${visita.direccion}</td>
+            <td>${visita.email}</td>
+            <td>${visita.motivo}</td>
+            <td>
+                <button onclick="editarVisita(${index})" class="btn btn-sm btn-primary">Editar</button>
+                <button onclick="borrarVisita(${index})" class="btn btn-sm btn-danger">Borrar</button>
+            </td>
+        `;
+    });
+}
+
+function editarVisita(index) {
+    let visitas = JSON.parse(localStorage.getItem('visitas')) || [];
+    const visita = visitas[index];
+
+    document.getElementById('nombre').value = visita.nombre;
+    document.getElementById('direccion').value = visita.direccion;
+    document.getElementById('email').value = visita.email;
+    document.getElementById('motivo').value = visita.motivo;
+
+
+    visitas.splice(index, 1);
+    localStorage.setItem('visitas', JSON.stringify(visitas));
+}
+
+function borrarVisita(index) {
+    let visitas = JSON.parse(localStorage.getItem('visitas')) || [];
+    visitas.splice(index, 1);
+    localStorage.setItem('visitas', JSON.stringify(visitas));
+    mostrarVisitas();
+}
+
+
+mostrarVisitas();
+document.getElementById('visitaForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+ 
+    const nombre = document.getElementById('nombre').value;
+    const direccion = document.getElementById('direccion').value;
+    const email = document.getElementById('email').value;
+    const motivo = document.getElementById('motivo').value;
+
+   
+    if (nombre.trim() === '' || direccion.trim() === '' || email.trim() === '' || motivo.trim() === '') {
+        alert('Por favor, completa todos los campos.');
+        return; 
     }
+
+
+    const visita = {
+        nombre,
+        direccion,
+        email,
+        motivo
+    };
+
   
-    function addTask() {
-      const taskValue = taskInput.value.trim();
-      if (taskValue !== '') {
-        let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-        tasks.push(taskValue);
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-        displayTasks();
-        taskInput.value = '';
-      } else {
-        alert('Por favor ingrese una tarea válida.');
-      }
-    }
-  
-    function editTask(index) {
-      let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-      const newTask = prompt('Editar tarea:', tasks[index]);
-      if (newTask !== null) {
-        tasks[index] = newTask.trim();
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-        displayTasks();
-      }
-    }
-  
-    function deleteTask(index) {
-      let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-      tasks.splice(index, 1);
-      localStorage.setItem('tasks', JSON.stringify(tasks));
-      displayTasks();
-    }
-  });
-  
+    let visitas = JSON.parse(localStorage.getItem('visitas')) || [];
+
+
+    visitas.push(visita);
+
+
+    localStorage.setItem('visitas', JSON.stringify(visitas));
+
+
+    mostrarVisitas();
+
+
+    alert('¡Visita agregada exitosamente!');
+});
